e39f108261b00ac96adc320dfc5cd15b3cf997f0
raiyan3341 <rayanbin13@gmail.com>
2025-12-10 20:02:18 +0600

    done

--- server.js
+++ server.js
@@ -13,25 +13,29 @@ const paymentRoutes = require('./routes/paymentRoutes'); // পেমেন্�
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
 const app = express();
+
 connectDB();
 
 app.use(express.json({ limit: '10mb' }));
 app.use(morgan('dev'));
-app.use(cors());
+app.use(cors()); 
+
 
 app.get('/', (req, res) => {
-    res.send('Course Enrollment API is running!'); 
+    res.send('Course Enrollment API is running successfully!'); 
 });
 
 app.get('/api', (req, res) => {
     res.json({ message: 'Welcome to the Course Enrollment API v1.0' });
 });
 
+
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/courses', courseRoutes);
 app.use('/api/enrollments', enrollmentRoutes);
 app.use('/api/payments', paymentRoutes);
+
 
 app.use(notFound);
 app.use(errorHandler);
