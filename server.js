8545f486b458b047026fff459c37ec75b1453684
raiyan3341 <rayanbin13@gmail.com>
2025-12-10 19:36:23 +0600

    done

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const handler = (req, res) => {
+    app(req, res);
+};
+
+module.exports = handler;
