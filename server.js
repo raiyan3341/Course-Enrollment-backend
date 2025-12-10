3d369ecd45fecb606719fa1e3cf13d068054690c
raiyan3341 <rayanbin13@gmail.com>
2025-12-10 19:57:17 +0600

    all done

--- server.js
+++ server.js
@@ -8,26 +8,24 @@ const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const courseRoutes = require('./routes/courseRoutes');
 const enrollmentRoutes = require('./routes/enrollmentRoutes');
-const paymentRoutes = require('./routes/paymentRoutes');
+const paymentRoutes = require('./routes/paymentRoutes'); // পেমেন্ট রুট যোগ করা হয়েছে
 
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
 const app = express();
 connectDB();
 
-app.get('/', (req, res) => {
-    res.send('Course Enrollment API is running!'); 
-});
-
-
-app.get('/api', (req, res) => {
-    res.json({ message: 'Welcome to the Course Enrollment API v1.0' });
-});
-
 app.use(express.json({ limit: '10mb' }));
 app.use(morgan('dev'));
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('Course Enrollment API is running!'); 
+});
+
+app.get('/api', (req, res) => {
+    res.json({ message: 'Welcome to the Course Enrollment API v1.0' });
+});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
@@ -35,13 +33,8 @@ app.use('/api/courses', courseRoutes);
 app.use('/api/enrollments', enrollmentRoutes);
 app.use('/api/payments', paymentRoutes);
 
-
 app.use(notFound);
 app.use(errorHandler);
 
-//const PORT = process.env.PORT || 5000;
-//const handler = (req, res) => {
-//    app(req, res);
-//};
 
 module.exports = app;
