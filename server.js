8493fba7c60cf47f455cf42ef9d93c08e077a05c
raiyan3341 <rayanbin13@gmail.com>
2025-12-10 19:52:46 +0600

    done

--- server.js
+++ server.js
@@ -15,6 +15,15 @@ const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 const app = express();
 connectDB();
 
+app.get('/', (req, res) => {
+    res.send('Course Enrollment API is running!'); 
+});
+
+
+app.get('/api', (req, res) => {
+    res.json({ message: 'Welcome to the Course Enrollment API v1.0' });
+});
+
 app.use(express.json({ limit: '10mb' }));
 app.use(morgan('dev'));
 app.use(cors());
